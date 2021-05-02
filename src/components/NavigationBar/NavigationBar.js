222482a33cef8ccac5c8e15fad8abcdc7b60e30e
ramadab-moutapha <moustapha.ramadan@hotmail.com>
2021-05-02 03:24:27 +0300

    navigationBar

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css'; // optional
 import { Section } from './Section/Section'
+import styles from "./styles.module.css"
 
 export class NavigationBar extends Component {
 
@@ -9,12 +10,18 @@ export class NavigationBar extends Component {
         return (
             <div>
                 <ul>
-                    <Section />
-                    <li><Tippy interactive={true}
-                        content={<Section />}
-                    ><button>My button</button></Tippy></li>
+                    <li>
+                        <Tippy interactive={true} content={<Section />} placement="bottom" arrow maxWidth={'100%'}>
+                            <div className="navBarItem">Resume Sections</div>
+                        </Tippy>
+                    </li>
+                    <li>
+                        <Tippy interactive={true} content={<div >Hello</div>} placement="bottom" arrow maxWidth={'100%'}>
+                            <div className="navBarItem">Hey</div>
+                        </Tippy>
+                    </li>
                 </ul>
-            </div>
+            </div >
         )
     }
 }
